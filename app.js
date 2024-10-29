f9143912f0db33bb453b2b36ce581c0fc5c59394
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2024-10-29 20:11:24 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const question = [
+    {
+        topic: "science",
+        question: "What color is the sky?",
+        possibleAnswer: ["blue", "green", "yellow"],
+        correctAnswer: "blue",
+    },
+    {
+        topic: "tech",
+        question: "What is the best language to learn?",
+        possibleAnswer: ["javascript", "python", "ruby"],
+        correctAnswer: "javascript",
+    },
+];
